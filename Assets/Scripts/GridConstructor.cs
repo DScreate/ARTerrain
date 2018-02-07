1af5a1541d0ca41150890f1abed0e6d70dc0f088
Unknown <blobzer@gmail.com>
2018-02-06 21:19:41 -0800

    Begin Canvas Coloring
    Initial commit of starting phase of procedural canvas and coloring algorithms

---
+++ Assets/Scripts/GridConstructor.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class GridConstructor : MonoBehaviour {
+
+    [Range(1,2000)]
+    public int xSize, ySize;
+
+    private Vector3[] vertices;
+
+    private Mesh mesh;
+
+    public void Awake()
+    {
+        StartCoroutine(Generate());
+    }
+
+    private IEnumerator Generate()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.01f);
+
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Grid";
+
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        for (int i = 0, y = 0; y <= ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                vertices[i] = new Vector3(x, y);
+                yield return wait;
+            }
+        }
+        mesh.vertices = vertices;
+
+        int[] triangles = new int[6];
+        triangles[0] = 0;
+        triangles[4] = triangles[1] = xSize + 1;
+        triangles[3] = triangles[2] = 1;
+        triangles[5] = xSize + 2;
+        mesh.triangles = triangles;
+
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+        {
+            return;
+        }
+
+        Gizmos.color = Color.black;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(transform.TransformPoint(vertices[i]), 0.1f);
+        }
+    }
+
+    /*
+    public void OnValidate()
+    {
+        Mathf.Clamp(xSize, 0, 2000);
+    }
+    */
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
