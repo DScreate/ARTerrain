feb7ac64ed689439e8851d504f703b4e0cb34ce0
Unknown <overlordofeverything@gmail.com>
2018-04-01 20:17:57 -0700

    Added Basic Linear Camera movement

---
+++ Assets/Scripts/Camera/CameraMover.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour {
+
+    public RailSystem rails;
+
+    private int currentSegment;
+    private float transition; //used in the raito in rail
+    private bool isCompleted;
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (!rails)
+            return;
+        if (!isCompleted)
+            Play();
+        
+	}
+
+    private void Play()
+    {
+        transition += Time.deltaTime * 1 / 2.5f;
+        if(transition > 1)
+        {
+            transition = 0;
+            currentSegment++;
+        }
+        else if (transition < 0)
+        {
+            transition = 1;
+            currentSegment--;
+        }
+
+        transform.position = rails.LinearPosition(currentSegment, transition);
+        transform.rotation = rails.Orientation(currentSegment, transition);
+    }
+}
---
+++ Assets/Scripts/Camera/RailSystem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RailSystem : MonoBehaviour {
+
+    private Transform[] railNodes;
+	// Use this for initialization
+	void Start () {
+        railNodes = GetComponentsInChildren<Transform>();
+ 
+	}
+
+    public Vector3 LinearPosition(int seg, float ratio)
+    {
+        Vector3 pointOne = railNodes[seg].position;
+        Vector3 pointTwo = railNodes[seg+1].position;
+
+        return Vector3.Lerp(pointOne,pointTwo,ratio);
+    }
+    public Quaternion Orientation(int seg, float ratio)
+    {
+        Quaternion quaterOne = railNodes[seg].rotation;
+        Quaternion quaterTwo = railNodes[seg+1].rotation;
+
+        return Quaternion.Lerp(quaterOne,quaterTwo,ratio);
+    }
+
+    private void OnDrawGizmos()
+    {
+        for(int i = 0; i < railNodes.Length - 1; i++)
+        {
+            Handles.DrawDottedLine(railNodes[i].position, railNodes[i + 1].position, 3.0f);
+        }
+    }
+
+}
