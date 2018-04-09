7c86dd0646b225ed78ee1950920a28172df29868
Unknown <blobzer@gmail.com>
2018-04-09 00:42:52 -0700

    Commit Multi-Menu UI
    Still requires Dynamic Texture Menu Creation to allow for different textures

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -7,12 +7,14 @@ public class MenuController : MonoBehaviour {
 
     WebcamTextureController webcamController;
     public DataForTerrain terrainData;
+    public NoiseData noiseData;
 
 	void Start () {
         webcamController = MapGenerator.webcamController;
 	    //terrainData = 
 	}
 
+    #region Webcam
     public void OnPlayButtonClick()
     {
         webcamController.Webcamtex.Play();
@@ -27,10 +29,45 @@ public class MenuController : MonoBehaviour {
     {
         webcamController.ChangeWebcamTextureToNextAvailable();
     }
-
+    #endregion
+    
+    #region Texture
     public void SetUniformScale(float vin)
     {
         terrainData.uniformScale = vin;
     }
+
+    public void SetHeightMultiplier(float vin)
+    {
+        terrainData.meshHeightMultiplier = vin;
+    }
+    #endregion
+
+    #region Noise
+    public void SetNoiseBlending(float vin)
+    {
+        //terrainData.
+    }
+
+    public void SetNoiseScale(float vin)
+    {
+        noiseData.noiseScale = vin;
+    }
+    
+    public void SetNoiseOctaves(float vin)
+    {
+        noiseData.octaves = Mathf.RoundToInt(vin);
+    }
+    
+    public void SetNoisePersistance(float vin)
+    {
+        noiseData.persistance = vin;
+    }
+    
+    public void SetNoiseLacunarity(float vin)
+    {
+        noiseData.lacunarity = vin;
+    }
+    #endregion
 
 }
