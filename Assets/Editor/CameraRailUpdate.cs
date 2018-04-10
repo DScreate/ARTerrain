0c68ec1767d6a259b2a611f0e53dbc801f22202d
Unknown <overlordofeverything@gmail.com>
2018-04-09 20:29:08 -0700

    Refined rail in a new scene and added update rail in editor

---
+++ Assets/Editor/CameraRailUpdate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CameraMovement;
+using UnityEditor;
+
+[CustomEditor(typeof(RailSystem), true)]
+    public class CameraRailUpdate : Editor
+    {
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            RailSystem data = (RailSystem)target;
+
+            if (GUILayout.Button("Update"))
+            {
+                data.setNodes();
+                EditorUtility.SetDirty(target);
+            }
+        }
+    }
